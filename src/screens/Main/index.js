ee0c80be280d3082e1274f75a3791855f02a1f54
Jinwoo Jeong (Leo) <thinkratomos@gmail.com>
2019-05-29 23:28:34 +0900

    Add frog

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -4,19 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={require("../../static/smoking.JPG")} alt="한번만 봐달라고요" />
+      <br />
+      한번만 봐달라고요
     </div>
   );
 }
